63a7f3819c1d56fc30dfd1e4cdf6130a866e9af4
Vignesh B <144299244+Vignesh100799@users.noreply.github.com>
2023-09-27 13:21:53 +0530

    Add files via upload

---
+++ Resume in JSON.js
@@ -0,0 +1,38 @@
+var resume = {
+    "name": "VIGNESH B",
+    "email": "baluvignesh422@gmail.com",
+    "phone":"+91 9080436341",
+    "OBJECTIVE": ["To secure a challenging position as a Full Stack Developer in a fast-paced environment", 
+    "where I can utilize my expertise in developing web applications and software solutions."],
+    "LOCATION": {
+      "address": "254/7, Ammal eari road 8th cross",
+      "postalCode": "636006",
+      "city": "Salem",
+      "District":"Salem",
+      "State":"Tamilnadu",
+     
+    },
+  
+  "EDUCATION": {
+    "college":"Thiagarajar polythechnic college",
+    "Degree": "D.P.E",
+    "Duration": "2014-2017",
+    "percentage":"82.5",
+    
+  },
+ 
+  "SKILLS": {
+    "name": "Data anotating skills, javascript, html, css",
+  },
+  "LANGUAGES": {
+    "language": "Tamil , English, kannada",
+  },
+  "INTEREST": {
+    "name": "Watching movie",
+    "keywords": 
+      "crime/thriller",
+  }  
+};
+for(var i in resume){
+    console.log(i,resume[i]);
+}
